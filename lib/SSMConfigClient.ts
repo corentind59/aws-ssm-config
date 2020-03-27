7bc0b3711f5aa40c04fe38e2d45f02391e65206a
Corentin Delannoy <corentin.delannoy2@gmail.com>
2020-03-27 15:52:46 +0100

    feat: added default config handling

--- lib/SSMConfigClient.ts
+++ lib/SSMConfigClient.ts
@@ -8,17 +8,26 @@ import { SSMConfigClientInitOptions } from './SSMConfigClientInitOptions';
  */
 export class SSMConfigClient<T> {
     /**
-     * AWS SSM client from aws-sdk module
+     * AWS SSM client from aws-sdk module.
      */
-    private ssm: SSM;
+    private readonly ssm?: SSM;
+
+    /**
+     * Default config to use when not in AWS environment.
+     */
+    private readonly config?: T;
 
     /**
      * Creates a SSM Client with the provided options.
      * @param options global options for the client. Each options can be overridden by the individual class' methods'
      * options parameter.
      */
-    constructor(private options: SSMConfigClientInitOptions) {
-        this.ssm = createSSM(options.capture ?? false);
+    constructor(private options: SSMConfigClientInitOptions<T>) {
+        if (options.config) {
+            this.config = options.config;
+        } else {
+            this.ssm = createSSM(options.capture ?? false);
+        }
     }
 
     /**
@@ -27,12 +36,24 @@ export class SSMConfigClient<T> {
      * @param options Can be used to override global options provided in the constructor.
      */
     public async getByKey<K extends keyof T>(key: K, options?: SSMConfigClientOptions): Promise<T[K]> {
+        if (this.config) {
+            return this.getLocalConfigByKey(key);
+        }
+
         const basePath = options?.basePath ?? this.options.basePath;
         const withDecryption = options?.withDecryption ?? this.options.withDecryption ?? false;
-        const params = await this.ssm.getParametersByPath({
+        const params = await this.ssm!.getParametersByPath({
             Path: basePath,
             WithDecryption: withDecryption
         }).promise();
         return params.Parameters!.find(({Name}) => Name === key) as T[K];
+    }
+
+    /**
+     * Retrieves a parameter from the default config object.
+     * @param key the key to search for in the config object.
+     */
+    private getLocalConfigByKey<K extends keyof T>(key: K): T[K] {
+        return this.config![key];
     }
 }
--- lib/SSMConfigClientInitOptions.ts
+++ lib/SSMConfigClientInitOptions.ts
@@ -3,9 +3,15 @@ import { SSMConfigClientOptions } from './SSMConfigClientOptions';
 /**
  * Interface describing options to be passed to {@link SSMConfigClient}'s constructor.
  */
-export interface SSMConfigClientInitOptions extends SSMConfigClientOptions {
+export interface SSMConfigClientInitOptions<T> extends SSMConfigClientOptions {
     /**
      * When true, every calls to SSM Parameter Store will be captured by XRay. Defaults to false.
      */
     capture?: boolean;
+
+    /**
+     * Set this field to config to be used when not in AWS environment. If this is set to null or false, or not set at
+     * all, it will fetch configuration from SSM Parameter Store (this is the default behavior).
+     */
+    config?: T | null | false;
 }
